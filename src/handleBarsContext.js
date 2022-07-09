5684431921b04009f7203c5cbcb51dcf1f7bfbe8
yatsinoa <sashayatzino@gmail.com>
2022-07-09 02:09:47 +0200

    header

--- src/handleBarsContext.js
+++ src/handleBarsContext.js
@@ -1,6 +1,54 @@
 export const handleBarsContext = {
 	navItems: [
-		{ href: '#', label: 'About us' },
-		{ href: '#', label: 'We are' },
+		{ href: '#', label: 'Mission' },
+		{ href: '#', label: 'Scent' },
+		{ href: '#', label: 'Mission Fragrances' },
+		{ href: '#', label: 'Unlock Your Full Potential' },
+		{ href: '#', label: 'Performance Enhancing Colognes' },
+		{ href: '#', label: 'Reviews' },
+		{ href: '#', label: 'Productivity App' },
+	],
+	honorTags: [
+		{
+			title: 'Bergamot',
+			description:
+				'Activates the motor nerve endings both in the conscious and subconscious mind, increasing the concentration power.',
+		},
+		{
+			title: 'Lemon and Petitgrain Citrus',
+			description: '',
+		},
+		{
+			title: 'Pepper Spice',
+			description: '',
+		},
+		{
+			title: 'Herbal Cypress and Shinus Moile',
+			description: '',
+		},
+		{
+			title: 'Neroli Floral',
+			description: '',
+		},
+		{
+			title: 'Nutmeg Spice',
+			description: '',
+		},
+		{
+			title: 'Musk',
+			description: '',
+		},
+		{
+			title: 'Top notes',
+			description: '',
+		},
+		{
+			title: 'Heart notes',
+			description: '',
+		},
+		{
+			title: 'Base notes',
+			description: '',
+		},
 	],
 };
